90d1650496e6a7f4f4c1086e27b5bec0017aaad8
EvilSevil666 <evilsevil666@gmail.com>
2022-10-19 22:11:02 +0300

    add [Z1-Z4]

---
+++ Z1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Программа которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее");
+Console.WriteLine("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine()!);
+
+if (number_1 > number_2)
+{
+    Console.WriteLine(number_1 + " больше чем " + number_2);
+}
+else
+{
+    Console.WriteLine(number_2 + " больше чем " + number_1);
+}
---
+++ Z2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Программа которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int number_3 = int.Parse(Console.ReadLine()!);
+
+int max = number_1;
+if (number_2 > max) max = number_2;
+if (number_3 > max) max = number_3;
+
+Console.WriteLine("Максимальное число " + max);
